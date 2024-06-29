0070166f87de1f3842eb24cb497fbfd2db8b1fe7
Bikram Gyawali <157723263+CoddingJourney@users.noreply.github.com>
2024-06-29 11:57:43 -0800

    Edit Section Complete

--- Blogs MS using Redux Toolkit/src/pages/blog/EditBlog.jsx
+++ Blogs MS using Redux Toolkit/src/pages/blog/EditBlog.jsx
@@ -52,10 +52,107 @@ const EditBlog = () => {
       dispatch(setEditStatus(null));
       navigate(`/blog/${id}`);
     }
-  });
+  }, [editstatus]);
   return (
     <Layout>
-      <Form type="Edit"  onSubmit={handleSubmit}/>
+      <form onSubmit={handleSubmit}>
+        <div className="max-w-2xl mx-auto p-4 bg-[#f2f2f2]">
+          <h2 className="text-center text-4xl mt-5 font-bold">Edit Blog</h2>{" "}
+          <br />
+          <div className="mb-6">
+            <label
+              htmlFor="title"
+              className="block text-lg font-medium text-gray-800 mb-1"
+            >
+              Title
+            </label>
+            <input
+              value={data.title}
+              type="text"
+              id="title"
+              name="title"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="subtitle"
+              className="block text-lg font-medium text-gray-800 mb-1"
+            >
+              Subtitle
+            </label>
+            <input
+              value={data.subtitle}
+              type="text"
+              id="subtitle"
+              name="subtitle"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="category"
+              className="block text-lg font-medium text-gray-800 mb-1"
+            >
+              Category
+            </label>
+            <input
+              type="text"
+              value={data.category}
+              id="category"
+              name="category"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="description"
+              className="block text-lg font-medium text-gray-800 mb-1"
+            >
+              Description
+            </label>
+            <textarea
+              id="description"
+              value={data.description}
+              name="description"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              rows="6"
+              onChange={handleChange}
+              required
+            ></textarea>
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="image"
+              className="block text-lg font-medium text-gray-800 mb-1"
+            >
+              Image
+            </label>
+            <input
+              type="file"
+              id="image"
+              name="image"
+              accept="image/*"
+              onChange={handleChange}
+              className="w-full"
+            />
+          </div>
+          <div className="flex justify-end">
+            <button
+              type="submit"
+              className="px-6 py-2 bg-indigo-500 text-white font-semibold rounded-md hover:bg-indigo-600 focus:outline-none"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
     </Layout>
   );
 };
